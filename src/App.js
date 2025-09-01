66827c1c08e2d08e0b57765e609c3d462dee2a93
mhilmicicek07 <m.hilmicicek_07@icloud.com>
2025-09-01 15:46:04 +0200

    update

--- src/App.js
+++ src/App.js
@@ -3,7 +3,30 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Hello World ! 
+      <div className='calculator-wrapper'>
+        <div className='screen-wrapper'>
+          <div className='prev-screen'>111111</div>
+          <div className='curr-screen'>111111</div>
+        </div>
+        <button className='ac'>AC</button>
+        <button>DEL</button>
+        <button>/</button>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button>*</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>+</button>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>-</button>
+        <button>,</button>
+        <button>0</button>
+        <button className='equal'>=</button>
+      </div>
     </div>
   );
 }
